6e73b54150c1db57d9119230cd8cd5c5126def4a
contatoyvesx <contato.yvesx@gmail.com>
2026-04-16 18:37:01 -0300

    Update site-layout.tsx

--- src/components/site-layout.tsx
+++ src/components/site-layout.tsx
@@ -4,10 +4,21 @@ import { SiteFooter } from "./site-footer";
 
 export function SiteLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="flex min-h-screen flex-col bg-background">
+    <div className="flex min-h-screen flex-col bg-background antialiased">
+      
+      {/* HEADER FIXO */}
       <SiteHeader />
-      <main className="flex-1">{children}</main>
+
+      {/* CONTEÚDO */}
+      <main className="flex-1 w-full">
+        <div className="mx-auto w-full max-w-[1400px] px-4 sm:px-6 lg:px-8">
+          {children}
+        </div>
+      </main>
+
+      {/* FOOTER */}
       <SiteFooter />
+
     </div>
   );
 }
